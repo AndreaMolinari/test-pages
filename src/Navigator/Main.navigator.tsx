9f1cc4f28cac14a7e4d52a74e21c10e1d3cdc0bf
AndreaMolinari <molinari91@gmail.com>
2022-11-24 19:40:17 +0100

    feat: aggiungo not found

--- src/Navigator/Main.navigator.tsx
+++ src/Navigator/Main.navigator.tsx
@@ -1,28 +1,38 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text } from "@rneui/themed";
 import React from "react";
 import Header from "../Elements/Header";
 import HomeView from "../HomeView";
 import BottomSheetComponent from "../LiFiZone";
-import * as Linking from "expo-linking";
-import { Text } from "@rneui/themed";
+import NotFound from "../NotFoundView";
+
+const NavStack = createNativeStackNavigator();
+
+const configs = {
+  screens: {
+    Home: "Home",
+    BottomSheet: "BottomSheet",
+    NoMatch: "*",
+  },
+};
 
 const linking = {
   prefixes: ["lifi-zone://", "https://andreamolinari.github.io/test-pages/"],
+  config: configs,
 };
-const NavStack = createNativeStackNavigator();
 
 export default () => {
   return (
     <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
       <NavStack.Navigator
-        initialRouteName="Home"
         screenOptions={{
           header: () => <Header />,
         }}
       >
         <NavStack.Screen component={HomeView} name="Home" />
         <NavStack.Screen component={BottomSheetComponent} name="BottomSheet" />
+        <NavStack.Screen component={NotFound} name="NoMatch" />
       </NavStack.Navigator>
     </NavigationContainer>
   );
---
+++ src/NotFoundView.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@rneui/themed";
+import React from "react";
+import Background from "./Elements/Background";
+
+const NotFound = () => {
+  return (
+    <Background>
+      <Text h1>Questa pagina non esiste</Text>
+    </Background>
+  );
+};
+
+export default NotFound;
